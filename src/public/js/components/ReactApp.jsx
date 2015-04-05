75dbdf3f5fbcc8f65c898226f8d85eb4e8dee9a9
Akash Pannu <akash5474@gmail.com>
2015-04-05 16:52:38 -0400

    Removed memory-stats legacy code

--- src/public/js/components/ReactApp.jsx
+++ src/public/js/components/ReactApp.jsx
@@ -4,25 +4,8 @@ import PromiseTasks from '../utils/promisetasks';
 import PromiseFlow from '../utils/promiseflow';
 
 const PromiseFlowComponent = FlowDemoFactory(PromiseTasks, PromiseFlow);
-// import GeneratorFlow from './GeneratorFlow.jsx';
-// import MemoryStats from 'memory-stats';
 
 export default class ReactApp extends React.Component {
-  // constructor() {
-  //   super();
-  //   var stats = new MemoryStats();
-
-  //   stats.domElement.style.position = 'fixed';
-  //   stats.domElement.style.right        = '0px';
-  //   stats.domElement.style.bottom       = '0px';
-
-  //   document.body.appendChild( stats.domElement );
-
-  //   requestAnimationFrame(function rAFloop(){
-  //       stats.update();
-  //       requestAnimationFrame(rAFloop);
-  //   });
-  // }
   render() {
     return (
       <section>
